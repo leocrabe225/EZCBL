fc13708237827c14f4832cb302611bdd2ad50774
leocrabe225 <petitscrounchs@yahoo.fr>
2026-01-07 06:49:40 +0100

    Working v0.1 - basic function call compilation

--- ezcbl.py
+++ ezcbl.py
@@ -2,17 +2,31 @@ import re
 import sys
 
 def compile_ezcbl(source:str):
-    output = ['Hello, COBOL!']
+    output:list[str] = []
 
     pattern = r'(\w+)\(([^)]+)\)'
-    match = re.search(pattern, source)
 
-    if match:
+    for match in re.finditer(pattern, source):
         func_name = match.group(1)
-        args = match.group(2)
+        args_str = match.group(2)
+        args = [arg.strip() for arg in args_str.split(',')]
 
-        print(f"Function: {func_name}")
-        print(f"Arguments: {args}")
+        #print(f"Function: {func_name}")
+        #print(f"Arguments: {args_str}")
+        #print(f"Arguments: {args}")
+
+        output.append(f"    PERFORM CALL-{func_name}")
+        output.append(f"")
+        output.append(f"CALL-{func_name}.")
+
+        for arg in args:
+            output.append(f"    MOVE {arg}")
+            output.append(f"      TO {arg} OF WS-PARAMS")
+            output.append(f"")
+
+        output.append(f"    CALL {func_name} USING WS-PARAMS")
+        output.append(f"    .")
+        output.append(f"")
 
     return '\n'.join(output)
 
